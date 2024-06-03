486399b1a77d26065cb6ee42b4750fb712ed2059
vizil <l.bolshev02@gmail.com>
2024-06-03 19:35:38 +0900

    s

--- src/Task.java
+++ src/Task.java
@@ -51,13 +51,4 @@ public class Task {
         return id == task.id;
     }
 
-    @Override
-    public String toString() {
-        return "Task{" +
-                "title='" + title + '\'' +
-                ", description='" + description + '\'' +
-                ", id=" + id +
-                ", status=" + status +
-                '}';
-    }
 }
